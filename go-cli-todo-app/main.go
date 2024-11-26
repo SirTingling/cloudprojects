778426880b74f9b3b184f10990ea7f983c2778e6
macbook <jordantingling6@gmail.com>
2024-11-26 15:39:04 -0500

    Cli-todo-app complete

--- go-cli-todo-app/main.go
+++ go-cli-todo-app/main.go
@@ -1,13 +1,23 @@
 package main
 
 import (
-// "fmt"
+	"fmt"
 )
 
 func main() {
 	todos := Todos{}
-	todos.add("Buy milk")
-	todos.add("Buy bread")
-	todos.toggle(0)
-	todos.print()
+	// Load data (todos) from storage:
+	storage := NewStorage[Todos]("todos.json")
+	err := storage.Load(&todos)
+	if err != nil {
+		fmt.Println("Warning: Could not load todos from storage. Starting fresh todos.")
+	}
+	// Parse & Execute
+	cmdFlags := NewCmdFlags()
+	cmdFlags.Execute(&todos)
+	// Save to storage
+	err = storage.Save(todos)
+	if err != nil {
+		fmt.Printf("Error saving todos in storage: %v\n", err)
+	}
 }
